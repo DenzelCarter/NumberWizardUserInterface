8b3f639e547b1ada8de48ffbde54673a488a3a3e
Denzel Carter <carter.denzel@yahoo.com>
2015-03-19 21:51:42 -0400

    Finished Game

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log ("New Level Load:" + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(string name){
+		Debug.Log ("Quit Loaded Level:");
+		Application.Quit();
+	}
+
+}
